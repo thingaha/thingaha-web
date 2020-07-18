729250d61949ab56d3cc97b77b81b51c57e60b96
dreamingblackcat <dreamingblackcat10@gmail.com>
2020-07-18 12:52:43 +0900

    Unused color styles cleanup

--- frontend/src/styles/variables.js
+++ frontend/src/styles/variables.js
@@ -1,39 +1,9 @@
 export const fontSource =
   'https://fonts.googleapis.com/css?family=Heebo:700|Muli:400,700,800&display=swap'
 
-export const color = {
-  bodyBackground: '#f5f5f5',
-  bodyColor: '#333',
-  modalOverlay: 'rgba(0, 0, 0, 0.5)',
-
-  backgroundLight: '#f5f5f5',
-  borderLight: '#e8e8e8',
-
-  primary: '#591a65',
-  primaryDark: '#491653',
-
-  black: '#393939',
-  gray: '#a3a3a3',
-  lightgray: '#e0e0e0',
-  darkgray: '#535353',
-
-  cta: '#29b2db',
-  ctaLight: 'rgba(41, 178, 219, 0.1)',
-
-  success: '#77c67a',
-  warning: '#ffb100',
-  error: '#ff0045',
-  errorLight: 'rgb(255, 0, 69, 0.1)',
-  connectionError: '#3c2845',
-  muted: '#b1b1b1',
-  mutedDark: '#a0a0a0',
-  mutedDarker: '#707070',
-  highlight: '#fff'
-}
-
 export const fontFamily = {
   base: 'Muli, Helvetica, Arial, sans-serif',
-  headings: 'Heebo, Helvetica, Arial, sans-serif'
+  headings: 'Heebo, Helvetica, Arial, sans-serif',
 }
 
 // These are all the font weights loaded from Google Fonts. Using others also requires updating
@@ -43,7 +13,7 @@ export const fontWeight = {
   bold: 700,
   extraBold: 800,
 
-  headingsBold: 700
+  headingsBold: 700,
 }
 
 export const fontSize = {
@@ -54,17 +24,17 @@ export const fontSize = {
   large: '1.125rem',
   small: '0.875rem',
   xsmall: '0.75rem',
-  xxsmall: '0.675rem'
+  xxsmall: '0.675rem',
 }
 
 export const borderRadius = {
   base: '0.25rem',
   large: '0.5rem',
-  round: '10rem'
+  round: '10rem',
 }
 
 export const transition = {
-  base: 'all 0.2s'
+  base: 'all 0.2s',
 }
 
 export const size = {
@@ -77,25 +47,27 @@ export const size = {
 
   gameWidthPortrait: '36rem',
   gameWidthLandscape: '56rem',
-  relatedCranesHeightMobile: '4rem'
+  relatedCranesHeightMobile: '4rem',
 }
 
 export const pxBreakpoints = {
   mobile: 0,
   tabletPortrait: 600,
   tabletLandscape: 1000,
-  desktop: 1200
+  desktop: 1200,
 }
 
 export const media = {
   mobileOnly: `@media (max-width: ${pxBreakpoints.tabletPortrait - 1}px)`,
   tabletPortraitUp: `@media (min-width: ${pxBreakpoints.tabletPortrait - 1}px)`,
-  tabletLandscapeUp: `@media (min-width: ${pxBreakpoints.tabletLandscape - 1}px)`,
-  desktopUp: `@media (min-width: ${pxBreakpoints.desktop - 1}px)`
+  tabletLandscapeUp: `@media (min-width: ${
+    pxBreakpoints.tabletLandscape - 1
+  }px)`,
+  desktopUp: `@media (min-width: ${pxBreakpoints.desktop - 1}px)`,
 }
 
 export const zIndex = {
   modalOverlay: 10000,
   slidingModal: 1000,
-  mobileFooterMenu: 999
+  mobileFooterMenu: 999,
 }
