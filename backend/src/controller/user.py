e1beb046d2141fc328907b9739d381f1300dd4fb
dreamingblackcat <dreamingblackcat10@gmail.com>
2020-10-15 22:21:51 +0900

    Make login end point return json in the same format as others Fix #56

--- backend/src/controller/user.py
+++ backend/src/controller/user.py
@@ -28,7 +28,8 @@ def login():
     if not user:
         return custom_error("Requested {} is not a registered member".format(email))
     if user_service.check_password(password, user):
-        access_token = create_access_token(identity=email, expires_delta=timedelta(days=1))
+        access_token = create_access_token(
+            identity=email, expires_delta=timedelta(days=1))
         return jsonify({
             "data": {
                 "access_token": access_token,
@@ -110,7 +111,8 @@ def create_user():
             "country": data.get("country"),
             "donation_active": data.get("donation_active")
         })
-        current_app.logger.info("Create user success. user_name %s", data.get("name"))
+        current_app.logger.info(
+            "Create user success. user_name %s", data.get("name"))
         return get_user_by_id(user_id)
     except (RequestDataEmpty, SQLCustomError, ValidateFail) as error:
         current_app.logger.error("Create user fail. user_name %s, error: %s",
@@ -152,11 +154,13 @@ def update_user(user_id: int):
             "status": user_update_status
         }), 200
     except ValueError as error:
-        current_app.logger.error("Value error for address id. error: %s", error)
+        current_app.logger.error(
+            "Value error for address id. error: %s", error)
         return jsonify({
             "errors": {[error.__dict__]}}), 400
     except (SQLCustomError, ValidateFail, RequestDataEmpty) as error:
-        current_app.logger.error("Fail to update user: %s, error: %s", user_id, error)
+        current_app.logger.error(
+            "Fail to update user: %s, error: %s", user_id, error)
         return jsonify({"errors": [error.__dict__]}), 400
 
 
