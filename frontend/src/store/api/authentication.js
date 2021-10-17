adca2f9153b7d4efb8a8bc407a481eb521649d5e
sisminnmaw <sisminnmaw@gmail.com>
2021-10-17 15:57:31 +0900

    Remove Draft Server from System
    -Remove Draft Server from Systems
    -add .env.local file
    -edit gitignore for .env.local file

--- frontend/src/store/api/authentication.js
+++ frontend/src/store/api/authentication.js
@@ -18,25 +18,21 @@ const draftLogin = ({ email_or_username, password }) => {
 }
 
 export const login = async ({ email_or_username, password }) => {
-  if (config.useDraftServer) {
-    return draftLogin({ email_or_username, password })
-  } else {
-    const { data } = await thingahaApiClient.post('/login', {
-      email_or_username,
-      password,
-    })
+  const { data } = await thingahaApiClient.post('/login', {
+    email_or_username,
+    password,
+  })
 
-    // Store the token in local storage for subsequent queries
-    const { access_token, user } = data
-    PersistentAuthentication.save({
-      accessToken: access_token,
-      currentUser: user,
-    })
+  // Store the token in local storage for subsequent queries
+  const { access_token, user } = data
+  PersistentAuthentication.save({
+    accessToken: access_token,
+    currentUser: user,
+  })
 
-    return {
-      accessToken: access_token,
-      currentUser: user,
-    }
+  return {
+    accessToken: access_token,
+    currentUser: user,
   }
 }
 
